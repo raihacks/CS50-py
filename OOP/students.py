910b89c98c4ea25232a9c94a5e9cc977cdb1bbf2
raihacks <siddhirai326@gmail.com>
2026-06-28 16:45:03 +0530

    5

---
+++ OOP/students.py
@@ -0,0 +1,71 @@
+'''
+classes = 
+'''
+
+# import random
+# class Hat:
+#     def __init__(self):
+#         self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+#     def sort(self, name):
+#         print(name, "is in", random.choice(self.houses))
+# hat = Hat()
+# hat.sort("Harry")
+
+# class Student:
+#     def __init__(self, name, house):
+#         self.name = name
+#         self.house = house
+# def main():
+#     student = get_student()
+#     print(f"{student.name} from {student.house}")
+# def get_student():
+#     name = input("Name: ")
+#     house = input("House: ")
+#     student = Student(name, house)
+#     return student
+# if __name__ == "__main__":
+#     main()
+
+# class Student :
+#     ...
+
+# def main():
+#     # name, house = get_student()
+#     student = get_student()
+#     print(f"{student.name} from {student.house}")
+
+# def get_student():
+#     student = Student() #object / instances
+#     student.name = input("Name: ") #attribut
+#     student.house = input("House: ") #attribut / instance variable
+#     return student
+
+# def get_student():
+#     # student = {}
+#     # student["name"] = input("Name: ")
+#     # student["house"] = input("House: ")
+#     # return student
+#     name = input("Name: ")
+#     house = input("House: ")
+#     return {"name": name, "house": house}
+
+# def get_student():
+#     name = input("Name: ")
+#     house = input("House: ")
+#     return (name, house) #tuple = unchangeable unmutable
+    # return [name, house] #list = changeable mutable
+
+# def main():
+#     name = get_name()
+#     house = get_house()
+#     print(f"{name} from {house}")
+
+# def get_name():
+#     return input("Name: ")
+
+# def get_house():
+#     return input("House: ")
+
+# if __name__ == "__main__":
+#     main()
